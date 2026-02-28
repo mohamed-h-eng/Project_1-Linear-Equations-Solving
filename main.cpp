a640ad27ee4168a33121a7ad557c31d1d3864b4c
mohamed-h-eng <mohamedhany1615@gmail.com>
2026-02-28 21:48:05 +0200

    final

--- main.cpp
+++ main.cpp
@@ -1,84 +1,136 @@
-/*
-    ------------------READ THIS--------------------
-    this a change log to track changes made in code
-
-        1.added code save const value after = sign 
-          from every equation in matrix_const
-*/
 # include <iostream>
 # include <string>
+#include <cmath> 
 using namespace std;
 
+void get_left_side(const string *input, string *temp_string) {
+    for(int i = 0; i < input->length(); i++) {
+        if((*input)[i] == '=') {
+            break;
+        } else {
+            *temp_string += (*input)[i];
+        }
+    }
+}
+
+void get_right_side(const string *input,int *matrix_const, int* index) {
+    string temp = "";
+    for(int i = 0; i < input->length(); i++) {
+        if((*input)[i] == '=') {
+            for(int k =i+1;k<input->length();k++){
+                temp += (*input)[k];
+            }
+        }
+    }
+    matrix_const[*index] = atof((temp).c_str());
+}
+
+void get_cooff(string *temp_string, int **matrix, int *eq_index){
+    int index = 0;
+    int var_count = 0;
+    string cooff = "";
+    for(int i=0;i<temp_string->length();i++){
+        if((*temp_string)[i] == 'x'){
+            // store chars till reaching x then 
+            for(int k=index;k<i;k++){
+                (cooff) += (*temp_string)[k];
+                index ++;
+            }
+            matrix[*eq_index][var_count] = atof((cooff).c_str());
+            (cooff) = "";
+            // skip char x1, x2 , ...
+            index ++;
+            index ++;
+            // count number of variables extracted (helps in storing values in right position in matrix)
+            var_count ++;
+        }
+    }
+}
+
+void parse_equation(string *equation, string *temp_string,int ** matrix,int *matrix_const, int* index){
+    get_left_side(equation, temp_string);
+    get_cooff(temp_string,matrix,index); // extract cooff and store in matrix depends on index->(equation number)
+    get_right_side(equation, matrix_const, index); // extract right side and store in matrix_const
+}
+
+void extend_matrix(int ** matrix, int ** matrix_ext, int *num_vars){
+    for(int i=0;i<*num_vars;i++){
+        for(int k=0; k<*num_vars;k++){
+            matrix_ext[i][k] = matrix[i][k];
+        }
+        for(int s=0; s<2;s++){
+            matrix_ext[i][s+*num_vars] = matrix[i][s];
+        }
+    }
+}
+
+void get_d_x(int ** matrix_ext,int **matrix, int * matrix_const, int *num_vars, int index){
+    for(int i =0;i<*num_vars;i++){
+        matrix_ext[i][index] = matrix_const[i];
+        for(int s=0; s<*num_vars-1;s++){
+            matrix_ext[i][s+*num_vars] = matrix_ext[i][s];
+        }
+    }
+}
+
+void get_positive_multiply(int **matrix_ext, int* result, int *num_vars) {
+    int multiply = 1;
+    for (int i = 0; i < *num_vars; i++) {       // each diagonal
+        multiply = 1;
+        for (int k = 0; k < *num_vars; k++) {   // each row
+            multiply *= matrix_ext[k][k + i];   // column shifts right by i
+        }
+        *result += multiply;
+    }
+}
+
+void get_negative_multiply(int **matrix_ext, int* result, int *num_vars) {
+    *result = 0;
+    int multiply = 1;
+    for (int i = *num_vars - 1; i < 2 * (*num_vars) - 1; i++) {  // start columns: 2,3,4
+        multiply = 1;
+        for (int k = 0; k < *num_vars; k++) {
+            multiply *= matrix_ext[k][i - k];   // anti-diagonal
+        }
+        *result -= multiply;
+    }
+}
+
 int main(){
-    string input;
     string equation;
-    int n;
-    cin >> n;
-    // int matrix[3][3];
-    // int matrix_const[3];
-    int** matrix = new int*[n];
-    for (int i = 0; i < n; i++) {
-        matrix[i] = new int[n];
+    string temp_string = "";
+    int num_vars;
+    int result = 0;
+    int D_value =0;
+    cin >> num_vars;
+    int** matrix = new int*[num_vars];
+    for (int i = 0; i < num_vars; i++) {
+        matrix[i] = new int[num_vars];
     }
-    int* matrix_const = new int[n];
-    int arr_index = 0;
-    string temp= "";
-    for(int i =0; i<n;i++){
-        cin >> equation;
-        
-        // save const value after = sign from every equation in matrix_const
-        for(int s =0;s<equation.length();s++){
-            if(equation[s] == '='){
-                temp = "";
-                for(int k=s+1;k<equation.length();k++){
-                    temp += equation[k];
-                }
-                cout << "const: " << temp << endl;
-                matrix_const[i] = atof(temp.c_str());
-                temp="";
-            }
-        }
-        // remove = sign and after
-        for(int i=0;i< equation.length();i++){
-            if(equation[i] =='='){
-                break;
-            }else{
-                temp +=equation[i];
-            }
-        }
-        temp;
-        int* arr = new int[n];
-        
-        // extract numbers and store in 2d array
-        // each equation extracted into array
-        // after all numbers extracted from one equation
-        // we store the array into one main array called matrix
-        int steps=0;
-        for(int i =0; i< temp.length();i++){
-            if(temp[i]=='x'){
-                arr[steps] = temp[i-1] - '0';
-                matrix[arr_index][steps] = arr[steps];
-                steps ++;
-            }
-        }
-        arr_index++;
-        }
-        // display 2d array(matrix)
-        cout << "\nextract numbers\n";
-        for (int i = 0; i < n; i++) {
-            for(int k =0; k<n;k++){
-                cout << matrix[i][k] << " ";
-            }
-            cout << endl;
+    int** matrix_ext = new int*[num_vars];
+    for (int i = 0; i < num_vars; i++) {
+        matrix_ext[i] = new int[2 * num_vars];
     }
-    // display const value after = sign
-    for(int i =0;i<n;i++){
-        cout << matrix_const[i] << " ";
+    int* matrix_const = new int[num_vars];
+    
+    //parsing the equations
+    for(int i =0; i<num_vars;i++){
+        cin >> equation;   
+        parse_equation(&equation, &temp_string,matrix,matrix_const, &i);
+        equation = "";
+        temp_string = "";
     }
-
-
-    // TODO
-    //      [DONE]store the constant of each equation in array
-    //      solve the matrix with Cramer’s rule
-    // ...
+    extend_matrix(matrix, matrix_ext, &num_vars);
+    get_negative_multiply(matrix_ext,&result,&num_vars);
+    get_positive_multiply(matrix_ext,&result,&num_vars);
+    D_value = result;
+    for(int i=0;i< num_vars;i++){
+        result = 0;
+        extend_matrix(matrix, matrix_ext, &num_vars);
+        get_d_x(matrix_ext,matrix,matrix_const,&num_vars,i);
+        get_negative_multiply(matrix_ext,&result,&num_vars);
+        get_positive_multiply(matrix_ext,&result,&num_vars);
+        cout << "x" << i+1 << "= " << result / D_value << endl;
+        result = 0;
+    }
 }
--- test.cpp
+++ test.cpp
@@ -1,37 +1,39 @@
 # include <iostream>
 # include <string>
+#include <cmath> 
 using namespace std;
 
-
-void get_left_side(const string *input, string *left_side, string *right_side) {
+void get_left_side(const string *input, string *temp_string) {
     for(int i = 0; i < input->length(); i++) {
         if((*input)[i] == '=') {
             break;
         } else {
-            *left_side += (*input)[i];
+            *temp_string += (*input)[i];
         }
     }
 }
 
-void get_right_side(const string *input, string *left_side, string *right_side) {
+void get_right_side(const string *input,int *matrix_const, int* index) {
+    string temp = "";
     for(int i = 0; i < input->length(); i++) {
         if((*input)[i] == '=') {
             for(int k =i+1;k<input->length();k++){
-                *right_side += (*input)[k];
+                temp += (*input)[k];
             }
         }
     }
+    matrix_const[*index] = atof((temp).c_str());
 }
 
-void get_cooff(string *left_side, int **matrix, int *eq_index){
-    int index = 0 ;
+void get_cooff(string *temp_string, int **matrix, int *eq_index){
+    int index = 0;
     int var_count = 0;
     string cooff = "";
-    for(int i=0;i<left_side->length();i++){
-        if((*left_side)[i] == 'x'){
+    for(int i=0;i<temp_string->length();i++){
+        if((*temp_string)[i] == 'x'){
             // store chars till reaching x then 
             for(int k=index;k<i;k++){
-                (cooff) += (*left_side)[k];
+                (cooff) += (*temp_string)[k];
                 index ++;
             }
             matrix[*eq_index][var_count] = atof((cooff).c_str());
@@ -45,59 +47,88 @@ void get_cooff(string *left_side, int **matrix, int *eq_index){
     }
 }
 
-void parse_equation(string *equation, string *left_side, string *right_side,int ** matrix, int* index){
-    get_left_side(equation, left_side, right_side);
-    get_right_side(equation, left_side, right_side);
-    get_cooff(left_side,matrix,index);
+void parse_equation(string *equation, string *temp_string,int ** matrix,int *matrix_const, int* index){
+    get_left_side(equation, temp_string);
+    get_cooff(temp_string,matrix,index); // extract cooff and store in matrix depends on index->(equation number)
+    get_right_side(equation, matrix_const, index); // extract right side and store in matrix_const
 }
 
 void extend_matrix(int ** matrix, int ** matrix_ext, int *n){
-    // extend matrix (copy the first columns except the last one 
-    // -- and position them at the end of matrix)
-    //for on the 3 equations
-    cout << "\n"; // display
     for(int i=0;i<*n;i++){
-        // get number
-        // copys the first 3 columns from matrix to matrix_ext
-        cout << "[ ";
         for(int k=0; k<*n;k++){
             matrix_ext[i][k] = matrix[i][k];
-            cout << matrix_ext[i][k] << " ";
-            // cout << "m[" <<i << "]" << "[" << k << "]" << "\n"; // display
         }
         // copy the first 2 columns from matrix to matrix_ext
         for(int s=0; s<2;s++){
-                matrix_ext[i][s+*n] = matrix[i][s];
-                cout << matrix_ext[i][s+*n] << " "; // display
-            // cout << "m[" <<i << "]" << "[" << s << "]" << "\n"; // display
-            }
-            //----the result should generate the matrix_ext with all values
-            // set number in matrix_ext
-        cout << "]\n";
+            matrix_ext[i][s+*n] = matrix[i][s];
+        }
     }
 }
 
-void get_positive_multiply(){
+void get_d_x(int ** matrix_ext,int **matrix, int * matrix_const, int *n, int index){
+    for(int i =0;i<*n;i++){
+        matrix_ext[i][index] = matrix_const[i];
+        // copy the first 2 columns from matrix to matrix_ext
+        for(int s=0; s<*n-1;s++){
+            matrix_ext[i][s+*n] = matrix_ext[i][s];
+        }
+    }
+}
+
+void get_positive_multiply(int **matrix_ext, int* result){
+    int zero = 0;
+    int multiply=1;
+    // int sum = 0;
+    // 2# get positive multiplications
+    cout << "\npositive multiply\n";
+    for(int i =0;i<3;i++){
+        for(int k=0;k<3;k++){
+            // first_res*=matrix_ext[zero][k+i];
+            cout << endl << "multiply: " << multiply << " * " << matrix_ext[zero][k+i] ;   
+            multiply *= matrix_ext[zero][k+i];
+            zero ++;
+        }
+        cout << endl << *result << " + " << multiply<< endl;
+        *result += multiply;
+        multiply =1;
+        zero = 0;
+    }
+    cout << endl;
+}
+
+void get_negative_multiply(int **matrix_ext, int* result){
+    int zero = 0;
+    int multiply=1;
+    *result = 0;
+    for(int i =4;i>1;i--){
+        for(int k=0;k<3;k++){
+            cout << endl << "multiply: " << matrix_ext[k][i-k] << " * " << multiply;
+            multiply*= matrix_ext[k][i-k];
+        }
+        cout << endl << *result << " - " << multiply<< endl;
+        *result =*result - multiply;
+        multiply=1;
+    }
 
 }
 
-void get_negative_multiply(){
-    
-}
+// void solve_cramer(int ** matrix, int ** matrix_ext, int *n){
+//     extend_matrix(matrix, matrix_ext, n);
+//     get_positive_multiply();
+//     get_negative_multiply();
+// }
 
-void solve_cramer(int ** matrix, int ** matrix_ext, int *n){
-    extend_matrix(matrix, matrix_ext, n);
-    get_positive_multiply();
-    get_negative_multiply();
 
-}
 int main(){
     string equation;
-    string left_side = "";
+    string temp_string = "";
     string right_side = "";
     string cooff= "";
+    
     int index=0;
     int n;
+    int result = 0; // result of final results like D_value or x1 , x2, ...
+    int D_value =0;
     cin >> n;
     // create an empty 2D matrix to store equations cooffecients
     int** matrix = new int*[n];
@@ -108,20 +139,17 @@ int main(){
     for (int i = 0; i < n; i++) {
         matrix_ext[i] = new int[n];
     }
-    // int matrix_ext[n][(((n-1)*2)+1)]={0}; // test case (use static array) update with(dynamic array)
-    // store equations constant at the right-side (after = sign)
     int* matrix_const = new int[n];
     
     //parsing the equations
     for(int i =0; i<n;i++){
         cin >> equation;   
-        parse_equation(&equation, &left_side, &right_side, matrix,&i);
+        parse_equation(&equation, &temp_string,matrix,matrix_const, &i);
         cout << "\nparsing equation " << i;
-        cout << "\nleft_side: " << left_side;
-        cout << "\nright_side: " << right_side << endl << endl;
+        cout << "\nleft_side: " << temp_string;
+        cout << "\nright_side: " << matrix_const[i] << endl << endl;
         equation = "";
-        left_side = "";
-        right_side = "";
+        temp_string = "";
     }
 
     // display 2d array(matrix)
@@ -133,6 +161,39 @@ int main(){
         cout << endl;
     }
 
-    // solve the matrix by cramers rule
-    solve_cramer(matrix, matrix_ext,&n);
+    // get d matrix and D_value
+    extend_matrix(matrix, matrix_ext, &n);
+    cout << "\ndisplay matrix D (extend)\n";
+    for (int i = 0; i < n; i++) {
+        cout << "[ ";
+        for(int k =0; k<((n-1)*2)+1;k++){
+            cout << matrix_ext[i][k] << " ";
+        }
+        cout << "]\n";
+    }
+
+    get_negative_multiply(matrix_ext,&result);
+    get_positive_multiply(matrix_ext,&result);
+    D_value = result;
+    cout << "d_value= " << D_value << endl;
+    
+    for(int i=0;i< n;i++){
+        // get d_x matrix and value
+        // extend_matrix(matrix, matrix_ext, &n);
+        cout << "\ndisplay matrix D_x" << i+1 << endl;
+        extend_matrix(matrix, matrix_ext, &n);
+        get_d_x(matrix_ext,matrix,matrix_const,&n,i);
+
+        for (int s = 0; s < n; s++) {
+            cout << "[ ";
+            for(int k =0; k<((n-1)*2)+1;k++){
+                cout << matrix_ext[s][k] << " ";
+            }
+            cout << "]\n";
+        }
+        get_negative_multiply(matrix_ext,&result);
+        get_positive_multiply(matrix_ext,&result);
+        cout << "x" << i+1 << "= " << result/D_value << endl;
+        result = 0;
+    }
 }
